24fa41ac5427abfbf1beac6f46e71bfc66fe8d89
Astitva Gupta <gupta.astitva@gmail.com>
2021-04-22 23:03:08 +0530

    Completed SCSR code and Ran the SC with learnt dictionary

--- SC/ScSR.py
+++ SC/ScSR.py
@@ -42,3 +42,59 @@ def lin_scale(xh, us_norm):
         s = us_norm * 1.2 / hr_norm
         xh = np.multiply(xh, s)
     return xh
+
+def ScSR(img_lr_y, size, upscale, Dh, Dl, lmbd, overlap):
+
+    patch_size = 3
+
+    img_us = resize(img_lr_y, size)
+    img_us_height, img_us_width = img_us.shape
+    img_hr = np.zeros(img_us.shape)
+    cnt_matrix = np.zeros(img_us.shape)
+
+    img_lr_y_feat = extract_lr_feat(img_hr)
+
+    gridx = np.append(create_list_step(0, img_us_width - patch_size - 1, patch_size - overlap), img_us_width - patch_size - 1)
+    gridy = np.append(create_list_step(0, img_us_height - patch_size - 1, patch_size - overlap), img_us_height - patch_size - 1)
+
+    count = 0
+
+    for m in tqdm(range(0, len(gridx))):
+        for n in range(0, len(gridy)):
+            count += 1
+            xx = int(gridx[m])
+            yy = int(gridy[n])
+
+            us_patch = img_us[yy : yy + patch_size, xx : xx + patch_size]
+            us_mean = np.mean(np.ravel(us_patch, order='F'))
+            us_patch = np.ravel(us_patch, order='F') - us_mean
+            us_norm = np.sqrt(np.sum(np.multiply(us_patch, us_patch)))
+
+            feat_patch = img_lr_y_feat[yy : yy + patch_size, xx : xx + patch_size, :]
+            feat_patch = np.ravel(feat_patch, order='F')
+            feat_norm = np.sqrt(np.sum(np.multiply(feat_patch, feat_patch)))
+
+            if feat_norm > 1:
+                y = np.divide(feat_patch, feat_norm)
+            else:
+                y = feat_patch
+
+            b = np.dot(np.multiply(Dl.T, -1), y)
+            w = fss_yang(lmbd, Dl, b)
+
+            hr_patch = np.dot(Dh, w)
+            hr_patch = lin_scale(hr_patch, us_norm)
+
+            hr_patch = np.reshape(hr_patch, (patch_size, -1))
+            hr_patch += us_mean
+
+            img_hr[yy : yy + patch_size, xx : xx + patch_size] += hr_patch
+            cnt_matrix[yy : yy + patch_size, xx : xx + patch_size] += 1
+
+    index = np.where(cnt_matrix < 1)[0]
+    img_hr[index] = img_us[index]
+
+    cnt_matrix[index] = 1
+    img_hr = np.divide(img_hr, cnt_matrix)
+
+    return img_hr
